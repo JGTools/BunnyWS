e6a9a8cebe010db67ef80507b93ccf4920fa7517
Erik Ekelöf <eribra@kth.se>
2023-01-13 12:45:33 +0100

    continued switching to bun built-in ws subscribe/pubish instead of broadcast

--- src/index.ts
+++ src/index.ts
@@ -1,49 +1,39 @@
 import { Server, ServerWebSocket } from "bun";
 
-/**
- * BunnyWSEvents is an interface that defines the event handlers for a BunnyWS server.
- */
+/** Interface that defines the event handlers for a BunnyWS server */
 export interface BunnyWSEvents {
     open: (ws: ServerWebSocket) => void;
-    message: (ws: ServerWebSocket, msg: string | Uint8Array) => void;
+    message: (ws: ServerWebSocket, msg: string | ArrayBufferView | ArrayBuffer) => void;
     close: (ws: ServerWebSocket) => void;
 }
 
-/**
- * BunnyWS is a WebSocket server.
- *
- * Properties:
- * - `clients` is a Map of all connected clients, with the client id as the key and the BunnyWSClient as the value.
- * - `broadcast` is a method that takes a message of type string or Uint8Array and sends it to all connected clients.
- */
+/** BunnyWS is a WebSocket server */
 export class BunnyWS {
+    private server;
     /**
-    * @param port - The port number to listen on.
-    * @param events - An object containing event handlers for the BunnyWS server.
+    * @param port - The port number to listen on
+    * @param events - An object containing event handlers for the BunnyWS server
     */
     constructor(port: number, events: BunnyWSEvents) {
-        Bun.serve({
+        this.server = Bun.serve({
             websocket: {
                 open(ws: ServerWebSocket) {
-                    ws.subscribe("room");
+                    ws.subscribe("global");
                     events.open(ws);
                 },
-                message(ws: ServerWebSocket, msg: string | Uint8Array) {
-                    events.message(ws, msg);
-                },
-                close(ws: ServerWebSocket) {
-                    events.close(ws);
-                }
+                message: events.message,
+                close: events.close,
             },
             fetch(req: Request, server: Server) {
-                if (!server.upgrade(req)) {
+                if (!server.upgrade(req))
                     return new Response(null, { status: 404 });
-                }
             },
             port
         });
     }
-    broadcast(ws: ServerWebSocket, msg: string | Uint8Array) {
-        ws.publish("room", msg);
+
+    /** Publishes a message to all connected clients */
+    publish(msg: string | ArrayBufferView | ArrayBuffer) {
+        this.server.publish("global", msg);
     }
 }
