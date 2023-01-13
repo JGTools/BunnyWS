63c82471d09a02c2bb1f18eace848b19106213c8
Erik Ekelöf <eribra@kth.se>
2023-01-13 12:11:23 +0100

    started switching to bun built-in ws subscribe/pubish instead of broadcast

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,12 @@
 import { Server, ServerWebSocket } from "bun";
 
 /**
- * BunnyWSClient is a ServerWebSocket and has an additional property `id` of type string, and a method `broadcast` which sends a message to all connected clients.
- */
-export type BunnyWSClient = ServerWebSocket<{
-    id: string;
-    broadcast: (msg: string | Uint8Array) => void;
-}>;
-
-/**
  * BunnyWSEvents is an interface that defines the event handlers for a BunnyWS server.
  */
 export interface BunnyWSEvents {
-    open: (ws: BunnyWSClient) => void;
-    message: (ws: BunnyWSClient, msg: string | Uint8Array) => void;
-    close: (ws: BunnyWSClient) => void;
+    open: (ws: ServerWebSocket) => void;
+    message: (ws: ServerWebSocket, msg: string | Uint8Array) => void;
+    close: (ws: ServerWebSocket) => void;
 }
 
 /**
@@ -25,36 +17,21 @@ export interface BunnyWSEvents {
  * - `broadcast` is a method that takes a message of type string or Uint8Array and sends it to all connected clients.
  */
 export class BunnyWS {
-    clients = new Map<string, BunnyWSClient>();
-    broadcast = (msg: string | Uint8Array) => {
-        for (const ws of this.clients.values()) {
-            ws.send(msg);
-        }
-    };
     /**
     * @param port - The port number to listen on.
     * @param events - An object containing event handlers for the BunnyWS server.
     */
     constructor(port: number, events: BunnyWSEvents) {
-        const clients = this.clients;
-        const broadcast = this.broadcast;
         Bun.serve({
             websocket: {
-                open(ws: BunnyWSClient) {
-                    ws.data = {
-                        id: crypto.randomUUID(),
-                        broadcast: broadcast
-                    };
-                    clients.set(ws.data.id, ws);
-
+                open(ws: ServerWebSocket) {
+                    ws.subscribe("room");
                     events.open(ws);
                 },
-                message(ws: BunnyWSClient, msg: string | Uint8Array) {
+                message(ws: ServerWebSocket, msg: string | Uint8Array) {
                     events.message(ws, msg);
                 },
-                close(ws: BunnyWSClient) {
-                    clients.delete(ws.data.id);
-
+                close(ws: ServerWebSocket) {
                     events.close(ws);
                 }
             },
@@ -65,5 +42,8 @@ export class BunnyWS {
             },
             port
         });
+    }
+    broadcast(ws: ServerWebSocket, msg: string | Uint8Array) {
+        ws.publish("room", msg);
     }
 }
